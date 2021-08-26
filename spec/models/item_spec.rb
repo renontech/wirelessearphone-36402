85d073e805b1e36b4a93d00888a2b61bfc7ffda5
renontech <pipipinon2552@gmail.com>
2021-08-26 16:47:19 +0900

    itemsテーブルに追加したカラムの単体テストコードを追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,15 @@ RSpec.describe Item, type: :model do
         @item.price = 29999
         expect(@item).to be_valid
       end
+      it 'sound, anc, ambient, type, size, codecが空でも登録できる' do
+        @item.sound = ''
+        @item.anc = ''
+        @item.ambient = ''
+        @item.type = ''
+        @item.size = ''
+        @item.codec = ''
+        expect(@item).to be_valid
+      end
     end
     context '登録できない時' do
       it 'nameが空だと登録できない' do
