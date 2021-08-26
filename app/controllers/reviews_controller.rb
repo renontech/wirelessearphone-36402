004d78792fee8864a176673a858042d9427ea4da
renontech <pipipinon2552@gmail.com>
2021-08-27 08:10:58 +0900

    createアクション作成
    - レビューコントローラーのnewアクションにインスタンス変数を定義してnewビューに渡す
    - レビューコントローラーのcreateアクションを作成
    - createのルーティングを記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,9 @@
 class ReviewsController < ApplicationController
   def new
+    @item = Item.find(params[:item_id])
+    @review = Review.new
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :show] do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :create]
   end
 end
