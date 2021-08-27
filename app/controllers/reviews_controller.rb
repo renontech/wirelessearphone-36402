a8ff28759369d59ed4fedf2f59686a4a21269bff
renontech <pipipinon2552@gmail.com>
2021-08-27 11:25:31 +0900

    レビュー投稿・一覧表示機能の実装
    - ストロングパラメーターの記述
    - 保存が成功するとイヤホン詳細ページに戻るよう記述
    - 詳細ページにレビューが一覧で表示されるように記述
    - before_actionでコントローラーの記述をまとめた

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,26 @@
 class ReviewsController < ApplicationController
+before_action :item_find
+
   def new
-    @item = Item.find(params[:item_id])
     @review = Review.new
   end
 
   def create
+    @review = Review.create(review_params)
+    if @review.save
+      redirect_to item_path(@item.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :text, :point).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:item_id])
   end
 end
--- db/migrate/20210826121852_create_reviews.rb
+++ db/migrate/20210826121852_create_reviews.rb
@@ -1,9 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.string      :title,    null: false
-      t.text        :text,     null: false
-      t.integer     :point,    null: false
+      t.string      :title, null: false
+      t.text        :text,  null: false
+      t.integer     :point, null: false
       t.references  :user,  null: false, foreign_key: true
       t.references  :item,  null: false, foreign_key: true
 
