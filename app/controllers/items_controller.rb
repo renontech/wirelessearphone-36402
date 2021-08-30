c92d2ca856240fff2f1778211de558e2d19c47dc
renontech <pipipinon2552@gmail.com>
2021-08-30 17:16:55 +0900

    イヤホンを追加登録時のフラッシュメッセージ追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.create(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'イヤホンを追加しました。'
     else
       render :new
     end
