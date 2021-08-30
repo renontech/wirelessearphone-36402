fca559656a5ca6d0fdf213f474cde7ad071cccab
renontech <pipipinon2552@gmail.com>
2021-08-30 21:58:22 +0900

    reviewのカラムを日本語化
    - テストコードを修正

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -18,37 +18,37 @@ RSpec.describe Review, type: :model do
       it 'titleが空だと投稿出来ない' do
         @review.title = ''
         @review.valid?
-        expect(@review.errors.full_messages).to include("Titleを入力してください")
+        expect(@review.errors.full_messages).to include("タイトルを入力してください")
       end
       it 'textが空だと投稿出来ない' do
         @review.text = ''
         @review.valid?
-        expect(@review.errors.full_messages).to include("Textを入力してください")
+        expect(@review.errors.full_messages).to include("本文を入力してください")
       end
       it 'pointが空だと投稿出来ない' do
         @review.point = ''
         @review.valid?
-        expect(@review.errors.full_messages).to include("Pointを入力してください")
+        expect(@review.errors.full_messages).to include("点数を入力してください")
       end
       it 'pointが半角数値でなければ投稿出来ない' do
         @review.point = '１０'
         @review.valid?
-        expect(@review.errors.full_messages).to include("Pointは数値で入力してください")
+        expect(@review.errors.full_messages).to include("点数は数値で入力してください")
       end
       it 'pointに小数点が含まれていると投稿出来ない' do
         @review.point = 2.5
         @review.valid?
-        expect(@review.errors.full_messages).to include("Pointは整数で入力してください")
+        expect(@review.errors.full_messages).to include("点数は整数で入力してください")
       end
       it 'pointが1未満の半角数値だと投稿できない' do
         @review.point = -1
         @review.valid?
-        expect(@review.errors.full_messages).to include("Pointは0以上の値にしてください")
+        expect(@review.errors.full_messages).to include("点数は0以上の値にしてください")
       end
       it 'pointが5を超える半角数値だと投稿できない' do
         @review.point = 6
         @review.valid?
-        expect(@review.errors.full_messages).to include("Pointは5以下の値にしてください")
+        expect(@review.errors.full_messages).to include("点数は5以下の値にしてください")
       end
       it 'userが紐づいていなければ投稿出来ない' do
         @review.user = nil
