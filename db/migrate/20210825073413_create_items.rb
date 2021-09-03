d791e534df6ac0aa90acdbd663a2539bdd47b00f
renontech <pipipinon2552@gmail.com>
2021-09-03 12:56:23 +0900

    マイグレーションファイルの修正

--- db/migrate/20210825073413_create_items.rb
+++ db/migrate/20210825073413_create_items.rb
@@ -1,15 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :name,    null: false
+      t.string  :name,    null: false, unique: true
       t.integer :price,   null: false
       t.string  :maker,   null: false
-      t.string  :sound
+      t.string  :driver
       t.string  :anc
       t.string  :ambient
-      t.string  :type
-      t.string  :size
+      t.string  :bluetooth
+      t.string  :weight
       t.string  :codec
+      t.date    :date
 
       t.timestamps
     end
