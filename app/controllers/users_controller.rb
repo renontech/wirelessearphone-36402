15034dffa3725d0d2f38f8f4b69afb6fa7fe656f
renontech <pipipinon2552@gmail.com>
2021-09-05 15:58:19 +0900

    お気に入りイヤホン表示
    - お気に入りに追加したイヤホンをマイページにリンク付きで表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.includes(:item)
+    @favorites = @user.favorites.includes(:item)
   end
 end
