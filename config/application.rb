b1dfae6084c113ef4c2dcac817e76cdf41043020
renontech <pipipinon2552@gmail.com>
2021-09-20 12:45:02 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Wirelessearphone36402
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
