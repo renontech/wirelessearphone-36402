d4c9ed01b5c25d9fe8d481afdacddb4f5b56e6ee
renontech <pipipinon2552@gmail.com>
2021-09-21 11:23:53 +0900

    Update application.js

--- app/assets/javascript/packs/application.js
+++ app/assets/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 //= require_tree .
 //= require jquery
 //= require jquery-ujs
-//= require rails-ujs
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
